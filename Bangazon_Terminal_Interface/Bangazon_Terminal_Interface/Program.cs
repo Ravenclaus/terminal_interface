a40a9aed643d8b0b9a566b68a0d8a2e7c81eb7dd
mgteutsch <mgt0117@gmail.com>
2017-04-04 17:42:27 -0700

    App.config contains connectionString. SQL folder moved into the project folder as well

---
+++ Bangazon_Terminal_Interface/Bangazon_Terminal_Interface/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bangazon_Terminal_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
