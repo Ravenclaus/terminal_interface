c3a3ddca7c65f03fdef106e1609e2df33553406f
jessica kilby <jessicakilby@gmail.com>
2017-04-08 12:39:48 -0500

    scripts and repository

--- Bangazon_Terminal_Interface/Bangazon_Terminal_Interface/Bangazon/ProductRepository.cs
+++ Bangazon_Terminal_Interface/Bangazon_Terminal_Interface/Bangazon/ProductRepository.cs
@@ -7,24 +7,51 @@ using System.Threading.Tasks;
 using Bangazon_Terminal_Interface.Bangazon.Models;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace Bangazon_Terminal_Interface.Bangazon
 {
     public class ProductRepository : IProductRepository
     {
-        IDbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RavenClausBangazon"].ConnectionString);
-
+        IDbConnection _productConnection;
 
         public ProductRepository()
         {
-          
+            _productConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RavenClausBangazon2"].ConnectionString);
         }
         
 
-        public List<Product> GetProduct()
+        public void AddProductId(int productId)
+        {
+            _productConnection.Open();
+
+            try
+            {
+                var addProductIdCommand = _productConnection.CreateCommand();
+                addProductIdCommand.CommandText = "Insert Into RavenClausBangazon2.dbo.Product(productId) Values(@productId)";
+                var idParameter = new SqlParameter("productId", SqlDbType.Int);
+                idParameter.Value = productId;
+                addProductIdCommand.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                _productConnection.Close();
+            }
+        }
+
+        public void AddProductName(string name)
         {
             throw new NotImplementedException();
         }
 
+        public void AddProductPrice(int price)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
