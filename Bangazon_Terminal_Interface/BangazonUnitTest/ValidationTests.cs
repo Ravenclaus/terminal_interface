bc984d98d97c4452f00110865978e2ead52ce3a9
sward42 <sarahward42@gmail.com>
2017-04-07 23:49:16 -0500

    add validation unit tests

--- Bangazon_Terminal_Interface/BangazonUnitTest/ValidationTests.cs
+++ Bangazon_Terminal_Interface/BangazonUnitTest/ValidationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bangazon_Terminal_Interface.Bangazon.Models;
 
 namespace BangazonUnitTest
 {
@@ -7,44 +8,82 @@ namespace BangazonUnitTest
     public class ValidationTests
     {
         [TestMethod]
-        public void EnsuretNameIsLetters()
+        public void EnsuretNameIsValid()
         {
+            Validation validation = new Validation();
+            string name = "John 08";
+
+            bool expectedResult = true;
+            bool actualResult = validation.ValidateName(name);
+
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.IsTrue(actualResult);
         }
 
         [TestMethod]
-        public void EnsureStreetIsLetters()
+        public void EnsureStreetIsValid()
         {
+            Validation validation = new Validation();
+            string street = "6578 Smith St";
 
+            bool actualResult = validation.ValidateAddress(street);
+
+            Assert.IsTrue(actualResult);
         }
 
         [TestMethod]
-        public void EnsureCityIsLetters()
+        public void EnsureCityIsValid()
         {
+            Validation validation = new Validation();
+            string city = "East Tawas";
 
+            bool actualResult = validation.ValidateCity(city);
+
+            Assert.IsTrue(actualResult);
         }
 
         [TestMethod]
-        public void EnsureStateIsCorrectFormat()
+        public void EnsureStateIsValid()
         {
+            Validation validation = new Validation();
+            string state = "MI";
 
+            bool actualResult = validation.ValidateState(state);
+
+            Assert.IsTrue(actualResult);
         }
 
         [TestMethod]
-        public void EnsureZipIsNumerals()
+        public void EnsureZipIsValid()
         {
+            Validation validation = new Validation();
+            string zip = "48833";
 
+            bool actualResult = validation.ValidateZip(zip);
+
+            Assert.IsTrue(actualResult);
         }
 
         [TestMethod]
-        public void EnsurePhoneIsCorrectFormat()
+        public void EnsurePhoneIsValid()
         {
+            Validation validation = new Validation();
+            string phone = "615-987-2373";
 
+            bool actualResult = validation.ValidatePhone(phone);
+
+            Assert.IsTrue(actualResult);
         }
 
         [TestMethod]
-        public void EnsureAcctIsNumerals()
+        public void EnsureCardIsValid()
         {
+            Validation validation = new Validation();
+            string cardNum = "3536 9808 6374 9846";
 
+            bool actualResult = validation.ValidateCard(cardNum);
+
+            Assert.IsTrue(actualResult);
         }
     }
 }
