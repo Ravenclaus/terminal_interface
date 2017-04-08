55b779057c650badf6a70ea55ea486487281dc0c
abbie <abigail.c.holder@gmail.com>
2017-04-08 11:25:12 -0500

    sorted out sql connections?

--- Bangazon_Terminal_Interface/Bangazon_Terminal_Interface/DAL/MathRepository.cs
+++ Bangazon_Terminal_Interface/Bangazon_Terminal_Interface/DAL/MathRepository.cs
@@ -22,12 +22,55 @@ namespace Bangazon_Terminal_Interface.DAL
 
         public void FindOrderTotal(int totalCost)
         {
-            throw new NotImplementedException();
+            _mathConnection.Open();
+
+            try
+            {
+                var addOrderTotalCommand = _mathConnection.CreateCommand();
+                addOrderTotalCommand.CommandText = "INSERT INTO RavenClausBangazon2.dbo.OrderTotalPrice(totalCost) VALUES(@totalCost)";
+                var totalCostParameter = new SqlParameter("totalCost", SqlDbType.Float);
+                totalCostParameter.Value = totalCost;
+                addOrderTotalCommand.Parameters.Add(totalCostParameter);
+
+                addOrderTotalCommand.ExecuteNonQuery();
+
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                _mathConnection.Close();
+            }
         }
 
         public void FindPopularity(int rank)
         {
-            throw new NotImplementedException();
+            _mathConnection.Open();
+
+            try
+            {
+                var addPopularityCommand = _mathConnection.CreateCommand();
+                // No column for Popularity in Products table, but do we need one, or are we just, a single time, finding the most popular item (times ordered)?
+                addPopularityCommand.CommandText = "INSERT INTO RavenClausBangazon2.dbo.Popularity(rank) VALUES(@rank)";
+                var rankParameter = new SqlParameter("rank", SqlDbType.Float);
+                rankParameter.Value = rank;
+                addPopularityCommand.Parameters.Add(rankParameter);
+
+                addPopularityCommand.ExecuteNonQuery();
+
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                _mathConnection.Close();
+            }
         }
     }
 }
